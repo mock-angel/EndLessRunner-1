f48461348d805ee7d82854312dd9bc2da569440d
Anantha Krishna R <sujakrishnar@gmail.com>
2020-02-29 08:19:44 +0530

    Prototype roof tile testing.

---
+++ EndLessRunner/Assets/Scripts/GeneratorScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.Tilemaps;
+
+public class GeneratorScript : MonoBehaviour
+{
+    //Building assets.
+    public GameObject roofTile;
+    public GameObject buildingTile;
+    
+    public GameObject player;
+    
+    bool newBuldingRequired = true;
+    
+    void Start(){
+        //Create start buildings.
+    }
+    
+    void FixedUpdate(){
+        //Check if more building placement is necessary.
+        
+        if(newBuldingRequired) {
+            CreateNewBuilding();
+            newBuldingRequired = false;
+        }
+    }
+    void createTiles(){
+        GameObject newObj = GameObject.Instantiate(roofTile, gameObject.transform.position, Quaternion.identity);
+        newObj.transform.parent = gameObject.transform;
+//        Destroy(newObj, 0.5f);
+//        Instantiate(roofTile, gameObject.transform.position, Quaternion.identity, gameObject);
+    }
+    
+    
+    void CreateNewBuilding(){
+        //Select Building type first.
+        
+        GameObject newBuilding = GameObject.Instantiate(new GameObject(), gameObject.transform.position, Quaternion.identity);
+        GameObject newTile = GameObject.Instantiate(roofTile, gameObject.transform.position, Quaternion.identity);
+//        newTile.alignment = (int)SpriteAlignment.TopLeft;
+    }
+    
+}
