15466c62d8c6f64dc6059710d2af18d44ff3bae9
Anantha Krishna R <sujakrishnar@gmail.com>
2020-03-01 18:40:29 +0530

    Scripts for saving and loading created. Prototype for loading created.

---
+++ EndLessRunner/Assets/Scripts/MenuLoad.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuLoad : MonoBehaviour
+{
+    public Player player;
+    
+    public string preText = "Coins : ";
+    
+    private Text textObj;
+    void Start()
+    {
+        textObj = gameObject.GetComponent<Text>();
+        if(player != null){
+            player.LoadPlayer();
+            
+            textObj.text = preText + player.Coins;
+        } else
+        {
+            Debug.LogError("player not set");
+        }
+    }
+    
+    void Update(){
+        textObj.text = preText + player.Coins;
+    }
+}
