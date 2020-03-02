76a70028f73efb35852af1dfc212383a4823766d
Anantha Krishna R <sujakrishnar@gmail.com>
2020-03-02 19:24:15 +0530

    Wrote scripts for Coins and Distance display on HUD

---
+++ EndLessRunner/Assets/Scripts/HUDScript/GamePlayCoinsDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GamePlayCoinsDisplay : MonoBehaviour
+{
+    public int coins;
+    
+    public ThisGameData thisGameData;
+    
+    private Text text;
+    
+    void Start(){
+        text = gameObject.GetComponent<Text>();
+    }
+    
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        coins = thisGameData.coinsCollected;
+        text.text = "Coins Collected \n" + coins;
+    }
+}
