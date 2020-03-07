bbbf4ee321f17ec5d3c20ad50621e746a20748ba
Anantha Krishna R <sujakrishnar@gmail.com>
2020-03-07 14:45:37 +0530

    Application exits on back button press.

---
+++ EndLessRunner/Assets/Scripts/SupportBackScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SupportBackScript : MonoBehaviour {
+ void Update(){
+  if (Application.platform == RuntimePlatform.Android)
+  {
+   if (Input.GetKey(KeyCode.Escape))
+   {
+    Application.Quit();
+   }
+  }
+ }
+}
